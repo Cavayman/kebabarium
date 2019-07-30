b06aac0f15a3191523984167c8d086a540d15a45
Oleg Kotsik <cavayman@gmail.com>
2019-07-30 18:39:01 +0300

    removed all shit, need to work again

--- tutorial-web/src/main/web/src/app/home/home.component.ts
+++ tutorial-web/src/main/web/src/app/home/home.component.ts
@@ -1,36 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { PlacesService } from '../shared/service/places.service';
 
 @Component({
   templateUrl: './home.component.html',
   styleUrls: ['home.css']
 })
 export class HomeComponent implements OnInit {
-  account: Account;
-  places: any;
-  randomItem: any;
-
-  constructor(private placesService: PlacesService) { }
-
-  ngOnInit() {
-    this.fetchPlaces();
+  ngOnInit(): void {
   }
 
-  selectRandom() {
-    this.placesService.loadPlacesList();
-    this.fetchPlaces();
-    this.getRandomArbitrary(0, this.places.size + 1);
-    console.log('random number is', this.getRandomArbitrary(0, this.places.size + 1));
-    this.randomItem = this.places[this.getRandomArbitrary(0, this.places.length + 1)];
-  }
+  constructor(){
 
-  fetchPlaces() {
-    this.placesService.getPlaces().subscribe(res => {
-      this.places = res;
-      console.log('MMAAAAAAIN', res);
-    });
-  }
-  getRandomArbitrary(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
   }
 }
--- tutorial-web/src/main/web/src/app/shared/service/places.service.ts
+++ tutorial-web/src/main/web/src/app/shared/service/places.service.ts
@@ -1,59 +1,50 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject } from 'rxjs';
 import {} from 'googlemaps';
 
 @Injectable()
 export class PlacesService {
-  API_URL: string;
-  pyrmont = new google.maps.LatLng(49.842957, 24.031111);
-  //map element
-  private map: any;
+
   //data store and observable
   private _places: BehaviorSubject<any>;
-  private dataStore: { todos: any } = { todos: [] };
+  private dataStore: { todos: any } ;
 
-  constructor(private http: HttpClient) {
-    this.API_URL = `https://maps.googleapis.com/maps/api/place/nearbysearch/json`;
+  constructor() {
     this._places = <BehaviorSubject<any>>new BehaviorSubject([]);
+    this.dataStore= { todos: [] };
   }
 
-  initMap(map) {
-    this.map = map;
-    console.log('map was inited', this.map);
-  }
-
-  getPlaces(): any {
+  getPlacesObservable(): any {
     return this._places.asObservable();
   }
 
-  loadPlacesList(): any {
-    console.log('loading places');
-    if (!this.map) {
-      console.log('sry your map is not initialized');
-    } else {
-      console.log('laod places list');
-      let map = new google.maps.Map(this.map, {
-        center: this.pyrmont,
-        zoom: 15
-      });
+  // loadPlacesList(): any {
+  //   console.log('loading places');
+  //   if (!this.map) {
+  //     console.log('sry your map is not initialized');
+  //   } else {
+  //     console.log('laod places list');
+  //     let map = new google.maps.Map(this.map, {
+  //       center: this.pyrmont,
+  //       zoom: 15
+  //     });
 
-      let request = {
-        location: this.pyrmont,
-        radius: 20000,
-        query: 'кебаб'
-      };
+  //     let request = {
+  //       location: this.pyrmont,
+  //       radius: 20000,
+  //       query: 'кебаб'
+  //     };
 
-      let service = new google.maps.places.PlacesService(map);
-      service.textSearch(request, (results, status) => {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-          for (var i = 0; i < results.length; i++) {
-            var place = results[i];
-            this.dataStore.todos.push(place);
-          }
-          this._places.next(Object.assign({}, this.dataStore).todos);
-        }
-      });
-    }
-  }
+  //     let service = new google.maps.places.PlacesService(map);
+  //     service.textSearch(request, (results, status) => {
+  //       if (status == google.maps.places.PlacesServiceStatus.OK) {
+  //         for (var i = 0; i < results.length; i++) {
+  //           var place = results[i];
+  //           this.dataStore.todos.push(place);
+  //         }
+  //         this._places.next(Object.assign({}, this.dataStore).todos);
+  //       }
+  //     });
+  //   }
+  // }
 }
